6ba49108c5c2958666f2c5c94e2903a01faa514d
higor-melo <higor.melo@tidewise.io>
2025-09-15 16:36:12 -0300

    revert: maintain '__' as a separator

--- bindings/ruby/lib/rock/gazebo.rb
+++ bindings/ruby/lib/rock/gazebo.rb
@@ -187,7 +187,9 @@ module Rock
         #   within this plugin need
         def self.process_gazebo_plugin(world, plugin_xml, loader:)
             scope = resolve_plugin_full_name(world.xml, plugin_xml)
-            normalize_rock_components(scope, plugin_xml, loader)
+            typekits = normalize_rock_components(scope, plugin_xml, loader)
+            plugin_xml.attributes["name"] = scope.gsub(/[^\w]/, "_")
+            typekits
         rescue OroGen::NotFound => e
             plugin_name = plugin_xml.attributes["name"]
             raise e, "while processing the <task ..> elements of the #{plugin_name} "\
