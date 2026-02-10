7e9dd9d12e26f69d1281117ddaae32934145ff01
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2026-02-10 13:27:02 -0300

    fix: make CameraTask pass

---
+++ bindings/ruby/test/test_plugin_CameraTask.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'test/helpers'
+
+describe 'gz_rock::CameraTask' do
+    include Orocos::Test::Component
+    include Helpers
+
+    Helpers.common_sensor_behavior(
+        self, world_basename: 'camera',
+              task_name: '/gazebo::w::m::l::c',
+              port_name: 'frame',
+              model_name: 'gz_rock::CameraTask'
+    )
+
+    def camera_configure_start_and_read_one_new_sample(
+        world, port_name = 'frame', nested_models_prefix: ""
+    )
+        @task = gzserver world, "/gazebo::w::m::#{nested_models_prefix}l::c"
+        yield(@task) if block_given?
+        configure_start_and_read_one_new_sample(port_name)
+    end
+
+    it 'exports the camera samples' do
+        camera_configure_start_and_read_one_new_sample 'camera.world', 'frame'
+    end
+end
